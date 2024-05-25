95bc3bf5efa8396413291cf5fc4c77d2f3eec7c0
Henry4593 <jonyango4@gmail.com>
2024-05-25 17:12:50 +0300

    solved tasks

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# script that sets up web servers for the deployment of web_static
+# Prepares the system for web application deployment
 sudo apt-get update
 sudo apt-get -y install nginx
 sudo ufw allow 'Nginx HTTP'
@@ -22,7 +22,7 @@ sudo ln -s -f /data/web_static/releases/test/ /data/web_static/current
 
 sudo chown -R ubuntu:ubuntu /data/
 
-has_location_block(){
+function has_location_block(){
     grep -q 'location /hbnb_static {' /etc/nginx/sites-enabled/default
 }
 
---
+++ 1-pack_web_static.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""This script generates a timestamped tar.gz archive of the 'web_static'
+directory.
+
+**Execution:**
+fab -f 1-pack_web_static.py do_pack
+This command uses Fabric to execute the `do_pack` function defined in this
+script.
+"""
+
+from datetime import datetime
+from fabric.api import *
+
+
+def do_pack():
+    """Creates a compressed archive (tar.gz) of the 'web_static' directory.
+    The archive filename incorporates a timestamp for easy identification 
+    and is placed in a subdirectory named 'versions'.
+
+    **Returns:**
+
+    - The filename of the created archive (string) on success.
+    - None if an error occurs during archive creation.
+    """
+    time = datetime.now()
+    time_format = time.strftime("%Y%m%d%H%M%S")
+    archive = "web_static_{}.tgz".format(time_format)
+    local('mkdir -p versions')
+    create_archive = local('tar -cvzf versions/{} web_static'.format(archive))
+    if create_archive is not None:
+        return archive
+    else:
+        return None
